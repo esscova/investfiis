9cee511ed289579df220974f6bfc1c3d901ac810
wellington moreira <wsantos08@hotmail.com>
2024-02-19 23:11:04 -0300

    refactor: layout, components, styles e responsividade.

--- src/components/AsideLogo.py
+++
@@ -1,9 +0,0 @@
-from dash import html
-
-layout = html.Div(
-    [
-        html.Img(src="../../assets/img/logo.png"),
-        html.H2("Fundos Imobiliários")
-    ],
-    className='logo'
-)
--- src/layouts/index.py
+++ src/layouts/index.py
@@ -3,40 +3,47 @@ from dash import html, dcc
 from services.getData import getData
 
 from src.components import (
-    AsideLogo,
     RangeSlider,
     Dropdown,
     DownloadBtn,
     Nav,
     Card,
 )
+
 df = getData()
 layout = html.Div(
-    [        
-        html.Aside(
+    [      
+        html.Title('Invest.Fiis'),
+        html.Link(rel='icon',type='image/x-icon',href='../../assets/img/favicon.png'),
+        html.Img(src="../../assets/img/logo.png",className='logo'),
+        html.Div(
             [
-                AsideLogo.layout,
+                html.H1('DASHBOARD'),
                 DownloadBtn.layout,
-                Nav.layout,
-            ]
+            ],
+            className='title'
         ),
-        html.Main(
+
+        Dropdown.layout,
+        RangeSlider.layout,
+
+        html.Div(
             [
-                # linha 1
-                Dropdown.layout,
-                RangeSlider.layout,
-                # linha 2
-                Card.layout('Fundos selecionados',html.Span(id='fundos-totais-selecionados')),
-                Card.layout('Não pagantes',df[df['DY']==0].shape[0]),
-                Card.layout('Descartados',df[df['DY']>37].shape[0]),
-                #linha 3
-                dcc.Graph(id="gr-fundos-selecionados"),
-                dcc.Graph(id="gr-top-10"),
-                dcc.Graph(id="gr-distribuicao"),
-                dcc.Graph(id="gr-means"),
-                Nav.layout,
-            ]
+                Card.layout(
+                    "Fundos selecionados",
+                    html.Span(id="fundos-totais-selecionados"),
+                ),
+                Card.layout("Não pagantes", df[df["DY"] == 0].shape[0]),
+                Card.layout("Descartados", df[df["DY"] > 37].shape[0]),
+            ],
+            className="cards",
         ),
+
+        dcc.Graph(id="gr-top-10"),
+        dcc.Graph(id="gr-distribuicao"),
+        dcc.Graph(id="gr-fundos-selecionados"),
+        dcc.Graph(id="gr-means"),
+        Nav.layout,
     ],
-    className="render",
+    className="root",
 )
